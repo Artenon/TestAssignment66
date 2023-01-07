c828a18cf887fb360140896c5e8fba599f09583b
Artem <artem_1parygin_2@mail.ru>
2023-01-07 17:49:08 +0500

    Add spinner when loading

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import PullToRefresh from 'react-simple-pull-to-refresh';
+import Loading from 'react-loading';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { fetchNews } from '../redux/api-actions';
-import { getNews, getStyles } from '../redux/selectors';
+import { getNews, getStyles, getIsLoading } from '../redux/selectors';
 import { Header } from '../components/header/header';
 import { Footer } from '../components/footer/footer';
 
@@ -11,6 +12,7 @@ export const News = (): JSX.Element => {
 
   const news = useAppSelector(getNews);
   const styles = useAppSelector(getStyles);
+  const isLoading = useAppSelector(getIsLoading);
 
   useEffect(() => {
     dispatch(fetchNews());
@@ -21,23 +23,26 @@ export const News = (): JSX.Element => {
   }
 
   return (
-    <PullToRefresh onRefresh={handleRefresh}>
       <div className='news'>
         <Header mainColor={styles?.mainColor} textColor={styles?.textColor} heading='Новости' />
         {
-          <ul className='news__items'>
-            {
-              news.map((item) => (
-                <li className='news__item' key={item.id} style={{backgroundColor: styles?.secondColor}}>
-                  <div className='news__title' style={{color: styles?.textColor}}>{item.title}</div>
-                  <div className='news__content' style={{color: styles?.textColor}}>{item.content}</div>
-                </li>
-              ))
-            }
-          </ul>
+          isLoading
+          ? <Loading type='spin' color={styles?.textColor} className='loading' />
+          : 
+          <PullToRefresh onRefresh={handleRefresh} pullDownThreshold={90} maxPullDownDistance={100} resistance={3}>
+            <ul className='news__items'>
+              {
+                news.map((item) => (
+                  <li className='news__item' key={item.id} style={{backgroundColor: styles?.secondColor}}>
+                    <div className='news__title' style={{color: styles?.textColor}}>{item.title}</div>
+                    <div className='news__content' style={{color: styles?.textColor}}>{item.content}</div>
+                  </li>
+                ))
+              }
+            </ul>
+          </PullToRefresh>
         }
         <Footer mainColor={styles?.mainColor} />
       </div>
-    </PullToRefresh>
   );
 };
